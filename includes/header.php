cd235bf851befff8ba8a53318ef51f43119afa6b
Michel Tüscher <mischu.tuescher@hotmail.com>
2016-01-22 08:30:48 +0100

    test nav

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,7 @@ $cart = $_SESSION['cart'];
 </head>
 <body>
 <header>
-    <nav id="navbar-big" class="navbar navbar-inverse">
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -38,52 +38,45 @@ $cart = $_SESSION['cart'];
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a id="shop-title" class="navbar-brand">Lu's <br> Cakes</a>
+                <a class="navbar-brand" href="#">Brand</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div id="navbar" class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul id="nav-left" class="nav navbar-nav">
-                    <li><a href="index.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['HOME']; ?></a></li>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $lang['PRODUCTS']; ?><span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="minicupcakes.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['MINI_CUPCAKES']; ?></a></li>
-                            <li><a href="cupcakes.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['CUPCAKES']; ?></a></li>
-                            <li><a href="cakes.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['CAKES']; ?></a></li>
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="products.php<?php echo $lang['LINK_LANG'];?>"><?php echo $lang['PRODUCTS']; ?></a></li>
+                            <li><a href="#">Separated link</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">One more separated link</a></li>
                         </ul>
                     </li>
-                    <li><a href="order.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['ORDER']; ?></a></li>
-                    <li><a href="aboutme.php<?php echo $lang['LINK_LANG']; ?>"><?php echo $lang['MENU_ABOUT_ME']; ?></a></li>
                 </ul>
-
+                <form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
                 <ul class="nav navbar-nav navbar-right">
-                    <?php
-                    if(!isset($_SESSION['userSession'])){
-                        ?><li><button type="submit" class="btn btn-default navbar-btn" name="btn-signup" onclick="location.href='login.php<?php echo $lang['LINK_LANG']; ?>'">
-                            <span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> &nbsp; Login
-                        </button></li><?php
-                    }else{
-                        ?><li><button type="submit" class="btn btn-default navbar-btn" name="btn-signup" onclick="location.href='logout.php<?php echo $lang['LINK_LANG']; ?>'">
-                        <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> &nbsp; Logout
-                        </button></li>
-                        <li><a href="login.php<?php echo $lang['LINK_LANG']; ?>'"><?php echo $username['username'] ?></a></li><?php
-                    }
-                    ?>
-                    <li><button type="submit" class="btn btn-default navbar-btn" name="btn-signup" onclick="location.href='show_cart.php<?php echo $lang['LINK_LANG']; ?>'">
-                        <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> &nbsp; <?php echo $lang['CART']; ?> <span class="badge"><?php $cart->countItem()?></span>
-                    </button></li>
+                    <li><a href="#">Link</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $lang['LANG']?><span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li><a href="?lang=de">DE</a></li>
-                            <li><a href="?lang=en">EN</a></li>
-                            <li><a href="?lang=fr">FR</a></li>
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
                         </ul>
                     </li>
-
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
